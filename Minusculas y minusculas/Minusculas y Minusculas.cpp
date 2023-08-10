3c85ef17bfce2273ebd274570dceb44472938b15
Mateo-chimarro <mschimarro@espe.edu.ec>
2023-08-09 21:44:20 -0500

    Deber V2.0.0

--- Minusculas y minusculas/Minusculas y Minusculas.cpp
+++ Minusculas y minusculas/Minusculas y Minusculas.cpp
@@ -14,43 +14,43 @@ void imprimirYGuardar(char *cadena, FILE *archivo) ;
 
 //Funcion para transformar una cadena a Mayusculas
 
-void Mayus(char cad[]){
-	int i=0;
-	while(cad[i]='\0'){	
-		
-		if(cad[i]>='a' && cad[i]<='z'){
-			cad[i]=cad[i] - ('a'-'A');	
-		}
-		i++;
-	}		
+
+void Mayus(char cad[]) {
+    int i = 0;
+    while (cad[i] != '\0') {
+        if (cad[i] >= 'a' && cad[i] <= 'z') {
+            cad[i] = cad[i] - ('a' - 'A');
+        }
+        i++;
+    }
 }
 
 //Funcion para transformar una cadena a Minusculas
 
-void Minus(char cad[]){
-	int i=0;
-	while(cad[i]='\0'){	
-		
-		if(cad[i]>='A' && cad[i]<='Z'){
-			cad[i]=cad[i] + ('a'-'A');	
-		}
-		i++;
-	}		
+void Minus(char cad[]) {
+    int i = 0;
+    while (cad[i] != '\0') {
+        if (cad[i] >= 'A' && cad[i] <= 'Z') {
+            cad[i] = cad[i] + ('a' - 'A');
+        }
+        i++;
+    }
 }
+
 //Funcion para transformar una cadena a Minusculas y Mayusculas
 
-void Minus_Mayus(char cad[], int tam){
-	int i=0;
-	for(int j=0; j<tam; j++){
-		
-		if(j%2==0){
-			
-            cad[i] = cad[i] - ('a' - 'A');
-			
-		}
-		i++;
-	}
-	
+void Minus_Mayus(char cad[], int tam) {
+    for (int i = 0; i < tam; i++) {
+        if (i % 2 == 0) {
+            if (cad[i] >= 'a' && cad[i] <= 'z') {
+                cad[i] = cad[i] - ('a' - 'A');
+            }
+        } else {
+            if (cad[i] >= 'A' && cad[i] <= 'Z') {
+                cad[i] = cad[i] + ('a' - 'A');
+            }
+        }
+    }
 }
 
 //Funcion para imprimir en el archivo
@@ -61,13 +61,13 @@ void imprimirYGuardar(char *cadena, FILE *archivo) {
     fprintf(archivo, "La cadena ingresada en mayusculas es: %s\n", cadena);
     Minus(cadena);
     fprintf(archivo, "La cadena ingresada en minusculas es: %s\n", cadena);
-    Minus_Mayus(cadena, strlen(cadena)); 
+    Minus_Mayus(cadena, strlen(cadena));
     fprintf(archivo, "La cadena alternada en mayusculas y minusculas es: %s\n", cadena);
     fprintf(archivo, "\n");
 }
 
-int main(int argc, char** argv) {
-   char cadena[50];
+int main() {
+    char cadena[50];
     printf("Ingrese una cadena: ");
     fgets(cadena, 50, stdin);
 
