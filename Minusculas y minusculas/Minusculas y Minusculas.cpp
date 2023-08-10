2960d75a9199390a44b68d7975eeae393150219d
Mateo-Chimarro <141870359+Mateo-Chimarro@users.noreply.github.com>
2023-08-09 21:24:08 -0500

    Update Minusculas y Minusculas.cpp

--- Minusculas y minusculas/Minusculas y Minusculas.cpp
+++ Minusculas y minusculas/Minusculas y Minusculas.cpp
@@ -1,7 +1,29 @@
 #include <iostream>
+#include <conio.h>
+#include <stdio.h>
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+//declaracion de prototipos
+
+
+//declaracion de funciones
+
+//Funcion para transformar una cadena a Mayusculas
+
+void Mayus(char cad[]){
+	int i=0;
+	while(cad[i]='\0'){	
+		
+		if(cad[i]>='a' && cad[i]<='z'){
+			cad[i]=cad[i] - ('a'-'A');	
+		}
+		i++;
+	}
+		
+}
 
 int main(int argc, char** argv) {
+
+	
+	
 	return 0;
 }
