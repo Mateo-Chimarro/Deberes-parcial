bc8ae73fe1ce1e3e79a82e7db311343be439b980
Mateo-Chimarro <141870359+Mateo-Chimarro@users.noreply.github.com>
2023-08-09 21:34:46 -0500

    Update Minusculas y Minusculas.cpp

--- Minusculas y minusculas/Minusculas y Minusculas.cpp
+++ Minusculas y minusculas/Minusculas y Minusculas.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
-#include <conio.h>
+#include <string.h>
+#include <stdlib.h>
 #include <stdio.h>
 
 //declaracion de prototipos
 
+void Mayus(char cad[]);
+void Minus(char cad[]);
+void Minus_Mayus(char cad[], int tam);
+void imprimirYGuardar(char *cadena, FILE *archivo) ;
 
 //declaracion de funciones
 
@@ -17,13 +22,66 @@ void Mayus(char cad[]){
 			cad[i]=cad[i] - ('a'-'A');	
 		}
 		i++;
-	}
+	}		
+}
+
+//Funcion para transformar una cadena a Minusculas
+
+void Minus(char cad[]){
+	int i=0;
+	while(cad[i]='\0'){	
 		
+		if(cad[i]>='A' && cad[i]<='Z'){
+			cad[i]=cad[i] + ('a'-'A');	
+		}
+		i++;
+	}		
+}
+//Funcion para transformar una cadena a Minusculas y Mayusculas
+
+void Minus_Mayus(char cad[], int tam){
+	int i=0;
+	for(int j=0; j<tam; j++){
+		
+		if(j%2==0){
+			
+            cad[i] = cad[i] - ('a' - 'A');
+			
+		}
+		i++;
+	}
+	
+}
+
+//Funcion para imprimir en el archivo
+
+void imprimirYGuardar(char *cadena, FILE *archivo) {
+    fprintf(archivo, "La cadena ingresada es: %s\n", cadena);
+    Mayus(cadena);
+    fprintf(archivo, "La cadena ingresada en mayusculas es: %s\n", cadena);
+    Minus(cadena);
+    fprintf(archivo, "La cadena ingresada en minusculas es: %s\n", cadena);
+    Minus_Mayus(cadena, strlen(cadena)); 
+    fprintf(archivo, "La cadena alternada en mayusculas y minusculas es: %s\n", cadena);
+    fprintf(archivo, "\n");
 }
 
 int main(int argc, char** argv) {
+   char cadena[50];
+    printf("Ingrese una cadena: ");
+    fgets(cadena, 50, stdin);
 
-	
-	
-	return 0;
+    FILE *archivo = fopen("cadenas.txt", "w");
+
+    if (archivo == NULL) {
+        printf("Error al abrir el archivo.\n");
+        return 1;
+    }
+
+    imprimirYGuardar(cadena, archivo);
+
+    fclose(archivo);
+    printf("La cadena se ha guardado en el archivo.\n");
+
+    return 0;
 }
